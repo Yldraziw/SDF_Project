715289daf3240f21203e946e7a2ca814cb22f8fe
Yldraziw <belairt@sheridancollege.ca>
2019-06-06 14:43:10 -0400

    Initial Commit - src folder Student_Demo
    Software Design Fundamentals - Term Project

---
+++ src/sdf_cardgameproject/SDF_CardGame.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sdf_cardgameproject;
+
+/**
+ *
+ * @author AGodW
+ */
+public class SDF_CardGame {
+    public static void main(String[] args) {
+        //TODO: Implement a card game of sorts to produce for Deliverable 1
+    }
+}
